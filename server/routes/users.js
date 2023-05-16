b31fbb09aff6f5ca6711a2c615e91d7c86b433ab
HuuPhuocHPH <luuvanhuuphuoc@gmail.com>
2023-05-16 10:20:25 +0700

    Verify token

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,21 @@
 import express  from "express";
 import { deleteUsers, getAllUserss, getUsers, updateUsers } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-// CREATE
+// CHECK - VERIFY
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+  res.send("hello user, you are logged in and you can delete your account")
+})
+
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+  res.send("hello admin, you are logged in and you can delete all accounts")
+})
 
 // UPDATE
 router.put("/:id", updateUsers)
