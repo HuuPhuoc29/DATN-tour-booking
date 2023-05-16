9b9353d3a8602e26c48f52821c7df36a9803f3f1
HuuPhuocHPH <luuvanhuuphuoc@gmail.com>
2023-05-16 10:30:29 +0700

    Verify roles

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,32 +1,33 @@
 import express  from "express";
+
 import { deleteUsers, getAllUserss, getUsers, updateUsers } from "../controllers/user.js";
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // CHECK - VERIFY
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("Hello user, you are logged in")
-})
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("Hello user, you are logged in")
+// })
 
-router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
-  res.send("hello user, you are logged in and you can delete your account")
-})
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
 
-router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
-  res.send("hello admin, you are logged in and you can delete all accounts")
-})
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
 
 // UPDATE
-router.put("/:id", updateUsers)
+router.put("/:id", verifyUser, updateUsers)
 
 // DELETE
-router.delete("/:id", deleteUsers)
+router.delete("/:id", verifyUser, deleteUsers)
 
 // GET
-router.get("/:id", getUsers)
+router.get("/:id", verifyUser, getUsers)
 
 // GET ALL
-router.get("/", getAllUserss)
+router.get("/", verifyAdmin, getAllUserss)
 
 export default router
